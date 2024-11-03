4bb91600bf01c38bdcc57aaa18f3fd5f9000d829
Alexander Tischler <114664256+AlexanderT97@users.noreply.github.com>
2024-11-03 14:52:29 +0100

    Update 5

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
