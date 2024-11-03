e0965aff709aa9f7b0db3ecba0d568f9f469d870
Alexander Tischler <114664256+AlexanderT97@users.noreply.github.com>
2024-11-03 14:26:51 +0100

    Update Fetch, CORS

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // MongoDB-Verbindung
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -78,6 +79,9 @@ app.get('/results', async (req, res) => {
     }
 });
 
+app.use(cors());
+
+app.use(express.json());
 
 // Server starten
 app.listen(PORT, () => {
