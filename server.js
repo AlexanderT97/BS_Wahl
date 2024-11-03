a520e2e2258334a2435b63bd0ba9ac22615e4c72
Alexander Tischler <114664256+AlexanderT97@users.noreply.github.com>
2024-11-03 14:46:52 +0100

    Update 3

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors());
+
+app.use(express.json());
+
 // MongoDB-Verbindung
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Verbunden mit MongoDB'))
@@ -79,9 +83,7 @@ app.get('/results', async (req, res) => {
     }
 });
 
-app.use(cors());
 
-app.use(express.json());
 
 // Server starten
 app.listen(PORT, () => {
