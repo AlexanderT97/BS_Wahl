a53cb952d65a325d8be55357d7b17989783a9e0c
Alexander Tischler <114664256+AlexanderT97@users.noreply.github.com>
2024-11-04 08:19:45 +0100

    Update 37

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',  // Erlaubt alle Ursprünge (nur für Entwicklung, in der Produktion am besten spezifische URLs angeben)
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  // Erlaubte HTTP-Methoden
+    allowedHeaders: ['Content-Type', 'Authorization']  // Erlaubte Header
+}));
 app.use(express.json());
 
 // MongoDB-Verbindung
