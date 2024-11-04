0e6a4f60016f673193efd6ff86e808bd499e39b9
Alexander Tischler <114664256+AlexanderT97@users.noreply.github.com>
2024-11-04 17:14:15 +0100

    Update 38

--- index.js
+++ index.js
@@ -13,9 +13,13 @@ app.use(cors({
 app.use(express.json());
 
 // MongoDB-Verbindung
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log('Verbunden mit MongoDB'))
-    .catch(err => console.error('Fehler bei der Verbindung mit MongoDB:', err));
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000
+})
+.then(() => console.log('Verbunden mit MongoDB'))
+.catch(err => console.error('Fehler bei der Verbindung mit MongoDB:', err));
 
 
 // Statische Dateien aus dem "public"-Ordner bereitstellen
